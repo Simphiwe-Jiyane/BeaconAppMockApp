e257e6e8d998fe6eba52e3c05671e7826b5a9993
Simphiwe Jiyane <jason.jiyane@gmail.com>
2021-05-17 23:31:48 +0200

    > Created skeleton for Directions helper method

--- app/src/main/java/com/example/beaconappmockapp/NetworkHelpers/DirectionsHelper.java
+++ app/src/main/java/com/example/beaconappmockapp/NetworkHelpers/DirectionsHelper.java
@@ -3,19 +3,38 @@ package com.example.beaconappmockapp.NetworkHelpers;
 //Create directions variables
 
 
+import com.example.beaconappmockapp.GoogleDirectionsHelpers.RoutesHelper;
+import com.example.beaconappmockapp.GooglePlacesHelpers.Places.GooglePlaceHelper;
+import com.google.android.gms.maps.model.LatLng;
+
 public class DirectionsHelper {
-    private String url = "https://maps.googleapis.com/maps/api/directions/json?origin=Disneyland&destination=Universal+Studios+Hollywood&key=AIzaSyBSXbvQ6E_PPDHtjtiIke1fOwM0PpkUekI";
-    String response;
+    //private String url = "https://maps.googleapis.com/maps/api/directions/json?origin=Disneyland&destination=Universal+Studios+Hollywood&key=AIzaSyBSXbvQ6E_PPDHtjtiIke1fOwM0PpkUekI";
 
 
     //Construct url
+    private String createUrl(LatLng origin, LatLng dest){
+
+        String url = "https://maps.googleapis.com/maps/api/directions/json?origin="+origin.latitude+","+origin.longitude+"&destination="+dest.latitude+","+dest.longitude+"&key=AIzaSyBSXbvQ6E_PPDHtjtiIke1fOwM0PpkUekI";
+
+        return url;
+    }
 
     //send an http request for json directions data and get a response
-    public void getResponse(){
+    public String getResponse(String url){
         NetworkUtil helper = new NetworkUtil(url);
-        this.response = helper.GetJsonResponse();
+        String response = helper.GetJsonResponse();
+        return response;
     }
 
     //TODO: Create Directions object and parse the JSON data
+
+    public RoutesHelper GetDirections(LatLng origin, LatLng dest){
+
+        String response = getResponse(createUrl(origin,dest));
+
+        //TODO: Break down the JSON response and return it to where it was called
+
+        return null;
+    }
 
 }
